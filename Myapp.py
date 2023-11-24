870d1fd3d1ec430d3aed0b0c55528b2f1988c1d7
Athitaya123 <athitayakakandee@gmail.com>
2023-11-24 07:13:50 +0700

    Update Myapp.py

--- Myapp.py
+++ Myapp.py
@@ -106,6 +106,9 @@ if st.button("ทำนายผล"):
    elif out[0]=="0":
       st.image('pic/N.jpg')
       st.header("No")
+   else:
+      st.image("./pic/N.jpg") 
+      st.header("No") 
    st.button("ไม่ทำนาย")
 
 else:
